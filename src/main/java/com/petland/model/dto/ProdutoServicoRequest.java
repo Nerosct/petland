7ff10773d545c327049c3b11571305a9677d2c9e
neros <noslinedsotnas72@gmail.com>
2026-08-02 23:09:47 -0300

    Feat: Criação de Dto para serviço

---
+++ src/main/java/com/petland/model/dto/ProdutoServicoRequest.java
@@ -0,0 +1,11 @@
+package com.petland.model.dto;
+
+import lombok.Data;
+
+@Data
+public class ProdutoServicoRequest {
+    private String nome;
+    private Double valor;
+    private boolean servico;
+    private Integer cliente;
+}
---
+++ src/main/java/com/petland/model/dto/ProdutoServicoResponse.java
@@ -0,0 +1,8 @@
+package com.petland.model.dto;
+
+import lombok.Data;
+
+@Data
+public class ProdutoServicoResponse extends ProdutoServicoRequest {
+    private Integer id;
+}
